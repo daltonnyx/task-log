93d30d1de0666fdcc8692a84f5dada54f86379fe
quytruong851@gmail.com <quytruong851@gmail.com>
2017-07-01 10:41:19 +0700

    Remove appController use repoFactory instead to load model

---
+++ server/repoFactory.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ENV = require('../env.js');
+
+class RepoFactory {
+    
+    constructor() {
+        this.models = [];   
+    }
+
+    load(model, params) {
+        params = params || {};
+        if(!this.models[model]) {
+            const modelClass = require(ENV.modelPath + `/${model}`);
+            this.models[model] = new modelClass(params); 
+        }
+        return this.models[model];
+    }
+}
+
+
+module.exports = new RepoFactory;
