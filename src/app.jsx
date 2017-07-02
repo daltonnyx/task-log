3d9a56032d817754cd48c24fafe47f8866502408
Quy Truong <aquez@thinkpadx200.aquez.io>
2017-07-02 15:25:09 +0700

    fetch data from server

--- src/app.jsx
+++ src/app.jsx
@@ -3,80 +3,24 @@ import Task from './task/task.js';
 import Sidebar from './sidebar.js';
 import Navbar from './nav.js';
 import TaskList from './task/taskList.js';
-const tasksData = [
-        {
-            _id: 1,
-            title: 'Fix web cho vantuongthanh',
-            description: 'Fix menu và mobile',
-            owner: {
-                fullName: 'Truong Ngoc Quy',
-                position: 'Leader',
-                username: 'quytn.vinabits',
-                password: '',
-                bio: '',
-                avatar: '',
-                company: {
-                    title: 'Vinabits',
-                    address: '',
-                },
-                role: {
-                    code: 'dev',
-                    caption: 'Developer',
-                }
-                
-            },
-            reward: {
-                score: 30,
-                title: 'fix web khach hang',
-
-            },
-            dateEnd: new Date('2017-05-23'),
-            priority: {
-                order: 1,
-                caption: 'Normal',
-            }
-        },
-        {
-            _id: 2,
-            title: 'Design web vinaphone',
-            description: 'Design giao diện mobile và desktop',
-            owner: {
-                fullName: 'Nguyen Thi Hien',
-                position: 'Leader',
-                username: 'hiennt.vinabits',
-                password: '',
-                bio: '',
-                avatar: '',
-                company: {
-                    title: 'Vinabits',
-                    address: '',
-                },
-                role: {
-                    code: 'design',
-                    caption: 'Designer',
-                }
-                
-            },
-            reward: {
-                score: 30,
-                title: 'fix web khach hang',
-
-            },
-            dateEnd: null,
-            priority: {
-                order: 1,
-                caption: 'Normal',
-            }
-        }
-];
 
 class App extends React.Component  {
     constructor() {
         super();
         this.completeTask = this.completeTask.bind(this);
         
-        var taskArray = tasksData.map( data => new Task(data) );
-        this.state = { tasks: taskArray };
+        this.state = { tasks: [] };
+    }
+
+    componentDidMount() {
+        fetch("/task/list",{
+            method: "GET"
+        }).then(response => {
+            console.log(response);
+            return response.json();
+        }).then(data => {
+            this.setState({tasks: data});
+        })
     }
 
     render() { 
@@ -100,12 +44,7 @@ class App extends React.Component  {
     }
 
     completeTask(task) {
-        
-        var tasks = this.state.tasks;
-        var completeTask = tasks.filter( t => t._id == task._id )[0];
-        completeTask.complete();
-        this.setState({tasks: tasks});
-        
+               
     }
 }
 
